bdaa4ebecc9d4bb95e94322615ef27004ed414b1
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-04 20:18:58 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,16 @@
+const allCategories = document.querySelectorAll(".item");
+const numberOfItems = allCategories.length;
+
+console.log(`Number of categories: ${numberOfItems} `);
+
+allCategories.forEach((element) => {
+  //console.log(element)
+
+  let elementTitle = element.querySelector("h2").textContent;
+
+  let oneTitleElements = element.querySelectorAll("li");
+  let oneTitleElemntsQty = oneTitleElements.length;
+
+  console.log("Category:", elementTitle);
+  console.log("Elements:", oneTitleElemntsQty);
+});
