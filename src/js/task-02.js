7b6939d8c8490b311497a814a4233417e465ca5b
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-04 21:21:16 +0300

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = ingredients.map((el) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = el;
+  return liEl;
+});
+
+document.querySelector("#ingredients").append(...list);
