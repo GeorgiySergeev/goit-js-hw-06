1fa0b275316fe2485424f11804562b0b31dd6feb
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-05 19:37:54 +0300

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+const span = document.querySelector("#value");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+function onDecrementBtnClick(event) {
+  counterValue -= 1;
+  span.textContent = counterValue;
+}
+
+function onIncrementBtnClick() {
+  counterValue += 1;
+  span.textContent = counterValue;
+}
+
+decrementBtn.addEventListener("click", onDecrementBtnClick);
+incrementBtn.addEventListener("click", onIncrementBtnClick);
