5ba0b50feaf97eabaf82eebee9b469cbf06fb95c
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-06 17:51:01 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
+console.log(formRef);
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget.elements;
+  const email = formElement.email.value;
+  const password = formElement.password.value;
+  const formAllData = {
+    email,
+    password,
+  };
+
+  if (email === "" || password === "") {
+    alert ("Please,fill in all fields!");
+  } else {
+    console.log(formAllData);
+    formRef.reset();
+  }
+};
